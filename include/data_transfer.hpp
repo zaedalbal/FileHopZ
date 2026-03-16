d7c58d06f978d68a2ec28253c14b36d94298722e
zaedalbal <vokiolhol@gmail.com>
2026-03-17 01:51:45 +0300

    сделал методы для отправки и получения пакетов

--- include/data_transfer.hpp
+++ include/data_transfer.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <boost/asio.hpp>
+#include "packet.hpp"
 
 class Data_transfer
 {
@@ -7,13 +8,15 @@ class Data_transfer
         explicit Data_transfer
         (boost::asio::io_context& context, unsigned short port)
         : context_(context),
-        socket_(context_, boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), port)){}
+        socket_(context_, boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), port))
+        {socket_.non_blocking(true);}
 
         explicit Data_transfer
         (boost::asio::io_context& context, const std::string& peer_adress, unsigned short peer_port)
         : context_(context),
         socket_(context_, boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), 0)),
-        peer_endpoint_(boost::asio::ip::make_address(peer_adress), peer_port){}
+        peer_endpoint_(boost::asio::ip::make_address(peer_adress), peer_port)
+        {socket_.non_blocking(true);}
 
         virtual ~Data_transfer() = default;
 
@@ -24,9 +27,11 @@ class Data_transfer
 
         virtual boost::system::error_code start_transfer() = 0;
 
-        virtual boost::system::error_code send_packet(); // пока что нет реализации
+        virtual boost::system::error_code send_packet(Packet* source, std::size_t* bytes_transferred_ptr); // пока что нет реализации
+        virtual boost::system::error_code send_packet(PacketHeader* source, std::size_t* bytes_transferred_ptr);
 
-        virtual boost::system::error_code receive_packet(); // пока что нет реализации
+        virtual boost::system::error_code receive_packet(Packet* destination, std::size_t* bytes_transferred_ptr); // пока что нет реализации
+        virtual boost::system::error_code receive_packet(PacketHeader* destination, std::size_t* bytes_transferred_ptr);
 
         // error код всегда пустой, он сделан чтобы в будущем можно было че то добавить и не менять возращаемый тип
         virtual boost::system::error_code print_progress(std::size_t bytes_transferred);
