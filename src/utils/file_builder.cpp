bf25b4d150c51cc68ce62c3f5e5d0f4daa438c39
zaedalbal <vokiolhol@gmail.com>
2026-03-29 05:59:29 +0300

    доделал реализацию методов в File_builder

--- src/utils/file_builder.cpp
+++ src/utils/file_builder.cpp
@@ -1,5 +1,13 @@
 #include "utils/file_builder.hpp"
 
+namespace
+{
+    inline std::filesystem::path extract_path(const Packet& packet)
+    {
+        return std::filesystem::path(std::string(packet.data, packet.header.size));
+    }
+}
+
 File_builder::File_builder(const std::filesystem::path& root)
 : root_(root)
 {
@@ -31,4 +39,63 @@ boost::system::error_code File_builder::create_file(const std::filesystem::path&
         return boost::system::errc::make_error_code(boost::system::errc::io_error);
     }
     return {};
+}
+
+
+boost::system::error_code File_builder::write(const char* data, std::size_t size, uint32_t file_id)
+{
+    auto it = open_files_.find(file_id);
+    if(it == open_files_.end())
+        return boost::system::errc::make_error_code(boost::system::errc::bad_file_descriptor);
+    it->second->write(data, static_cast<std::streamsize>(size)); // static_cast на будущее, если будет передаваться большие объемы данных
+    if(it->second->bad() || it->second->fail()) // проверка ошибки записи
+        return boost::system::errc::make_error_code(boost::system::errc::io_error);
+    return {};
+}
+
+
+boost::system::error_code File_builder::close_file(uint32_t file_id)
+{
+    auto it = open_files_.find(file_id);
+    if(it == open_files_.end())
+        return boost::system::errc::make_error_code(boost::system::errc::bad_file_descriptor);
+    it->second->close();
+    open_files_.erase(it);
+    return {};
+}
+
+boost::system::error_code File_builder::handle_packet(const Packet& packet)
+{
+    switch (packet.header.type)
+    {
+        case PacketType::KEEP_ALIVE:
+        case PacketType::CONFIRM:
+        case PacketType::CONFIRM_FAILED:
+
+        case PacketType::CREATE_DIRECTORY:
+        {
+            return create_directory(extract_path(packet));
+        }
+
+        case PacketType::CREATE_FILE:
+        {
+            std::unique_ptr<std::ofstream> file;
+            auto ec = create_file(extract_path(packet), file);
+            if(ec)
+                return ec;
+            open_files_[packet.header.file_id] = std::move(file);
+            return {};
+        }
+        case PacketType::FILE_DATA:
+        {
+            return write(packet.data, packet.header.size, packet.header.file_id);
+        }
+        case PacketType::END_FILE:
+        case PacketType::END_TRANSFER:
+
+        default:
+        {
+            return {};
+        }
+    }
 }
