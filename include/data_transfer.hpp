cfb5cd33f5c62a46a1d70cc26525c7fb03056221
zaedalbal <vokiolhol@gmail.com>
2026-04-04 13:42:43 +0300

    переписал Data_transfer на корутины

--- include/data_transfer.hpp
+++ include/data_transfer.hpp
@@ -21,21 +21,90 @@ class Data_transfer
 
         virtual ~Data_transfer() = default;
 
-        virtual boost::system::error_code start() = 0;
+        virtual boost::asio::awaitable<boost::system::error_code> 
+        start() = 0;
 
     protected:
-        virtual boost::system::error_code transfer_confirmation() = 0;
+        virtual boost::asio::awaitable<boost::system::error_code>
+        transfer_confirmation() = 0;
 
-        virtual boost::system::error_code start_transfer() = 0;
+        virtual boost::asio::awaitable<boost::system::error_code>
+        start_transfer() = 0;
 
-        virtual boost::system::error_code send_packet(Packet* source, std::size_t* bytes_transferred_ptr);
-        virtual boost::system::error_code send_packet(PacketHeader* source, std::size_t* bytes_transferred_ptr);
+        template<typename T>
+        boost::asio::awaitable<boost::system::error_code>
+        send_packet(const T* source, std::size_t* bytes_transferred_ptr)
+        {
+            static_assert(std::is_same_v<T, Packet> || std::is_same_v<T, PacketHeader>,
+            "Unsupporetd type");
 
-        virtual boost::system::error_code receive_packet(Packet* destination, std::size_t* bytes_transferred_ptr);
-        virtual boost::system::error_code receive_packet(PacketHeader* destination, std::size_t* bytes_transferred_ptr);
+            boost::system::error_code ec;
+
+            if(!source)
+                co_return boost::asio::error::invalid_argument;
+            
+            std::size_t buffer_size;
+
+            if constexpr(std::is_same_v<T, Packet>)
+            {
+                if(source->header.size > PACKET_SIZE)
+                    co_return boost::asio::error::message_size;
+                buffer_size = sizeof(PacketHeader) + source->header.size;
+            }
+            else
+            {
+                if(source->size > PACKET_SIZE)
+                    co_return boost::asio::error::message_size;
+                buffer_size = sizeof(PacketHeader);
+            }
+
+            auto bytes_transferred = co_await socket_.async_send_to
+            (boost::asio::buffer(source, buffer_size),
+            peer_endpoint_,
+            boost::asio::redirect_error(boost::asio::use_awaitable, ec));
+
+            if(bytes_transferred_ptr)
+                *bytes_transferred_ptr = bytes_transferred;
+            
+            co_return ec;
+        }
+
+        template<typename T>
+        boost::asio::awaitable<boost::system::error_code>
+        receive_packet(T* destination, std::size_t* bytes_transferred_ptr)
+        {
+            static_assert(std::is_same_v<T, Packet> || std::is_same_v<T, PacketHeader>,
+            "Unsupporetd type");
+
+            boost::system::error_code ec;
+
+            if(!destination)
+                co_return boost::asio::error::invalid_argument;
+            
+            std::size_t buffer_size = sizeof(T);
+
+            auto bytes_transferred = co_await socket_.async_receive_from
+            (boost::asio::buffer(destination, buffer_size),
+            peer_endpoint_,
+            boost::asio::redirect_error(boost::asio::use_awaitable, ec));
+            
+            if constexpr(std::is_same_v<T, PacketHeader>)
+            {
+                if(destination->size > PACKET_SIZE)
+                    co_return boost::asio::error::message_size;
+            }
+            else
+                if(destination->header.size > PACKET_SIZE)
+                    co_return boost::asio::error::message_size;
+            if(bytes_transferred_ptr)
+                *bytes_transferred_ptr = bytes_transferred;
+
+            co_return ec;
+        }
 
         // error код всегда пустой, он сделан чтобы в будущем можно было че то добавить и не менять возращаемый тип
-        virtual boost::system::error_code print_progress(std::size_t bytes_transferred);
+        virtual boost::system::error_code
+        print_progress(std::size_t bytes_transferred);
 
     protected:
         boost::asio::io_context& context_;
--- src/data_transfer.cpp
+++ src/data_transfer.cpp
@@ -1,60 +1,6 @@
 #include "data_transfer.hpp"
 #include <iostream>
 
-boost::system::error_code Data_transfer::send_packet(PacketHeader* source, std::size_t* bytes_transferred_ptr)
-{
-    boost::system::error_code ec;
-    if(!source)
-        return boost::asio::error::invalid_argument;
-    if(source->size > PACKET_SIZE)
-        return boost::asio::error::message_size;
-    auto bytes_transferred = socket_.send_to(boost::asio::buffer(source, sizeof(PacketHeader)),
-    peer_endpoint_, 0, ec);
-    if(bytes_transferred_ptr)
-        *bytes_transferred_ptr = bytes_transferred;
-    return ec;
-}
-
-boost::system::error_code Data_transfer::send_packet(Packet* source, std::size_t* bytes_transferred_ptr)
-{
-    boost::system::error_code ec;
-    if(!source)
-        return boost::asio::error::invalid_argument;
-    if(source->header.size > PACKET_SIZE)
-        return boost::asio::error::message_size;
-    auto bytes_transferred = socket_.send_to(boost::asio::buffer(source, sizeof(PacketHeader) + source->header.size),
-    peer_endpoint_, 0, ec);
-    if(bytes_transferred_ptr)
-        *bytes_transferred_ptr = bytes_transferred;
-    return ec;
-}
-
-boost::system::error_code Data_transfer::receive_packet(PacketHeader* destination, std::size_t* bytes_transferred_ptr)
-{
-    boost::system::error_code ec;
-    if(!destination)
-        return boost::asio::error::invalid_argument;
-    auto bytes_transferred = socket_.receive_from(boost::asio::buffer(destination, sizeof(PacketHeader)), peer_endpoint_, 0, ec);
-    if(destination->size > PACKET_SIZE)
-        return boost::asio::error::message_size;
-    if(bytes_transferred_ptr)
-        *bytes_transferred_ptr = bytes_transferred;
-    return ec;
-}
-
-boost::system::error_code Data_transfer::receive_packet(Packet* destination, std::size_t* bytes_transferred_ptr)
-{
-    boost::system::error_code ec;
-    if(!destination)
-        return boost::asio::error::invalid_argument;
-    auto bytes_transferred = socket_.receive_from(boost::asio::buffer(destination, sizeof(Packet)), peer_endpoint_, 0, ec);
-    if(destination->header.size > PACKET_SIZE)
-        return boost::asio::error::message_size;
-    if(bytes_transferred_ptr)
-        *bytes_transferred_ptr = bytes_transferred;
-    return ec;
-}
-
 boost::system::error_code Data_transfer::print_progress(std::size_t bytes_transferred)
 {
     static std::size_t total_bytes_transferred = 0;
