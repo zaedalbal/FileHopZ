3cef101118982a5d569e666cb2988ee0165394de
zaedalbal <vokiolhol@gmail.com>
2026-04-23 03:15:15 +0300

    поменял интерфейс File_builder

--- include/utils/file_builder.hpp
+++ include/utils/file_builder.hpp
@@ -12,15 +12,11 @@ class File_builder
         // конструктор принимает имя корневой дериктории и создает её, если её нет
         explicit File_builder(const std::filesystem::path& root);
 
-        // метод для обработки пакета
-        boost::system::error_code handle_packet(const Packet& packet);
-
-    private:
         // метод для создания директории
         boost::system::error_code create_directory(const std::filesystem::path& relative_path);
 
         // метод для создания файла
-        boost::system::error_code create_file(const std::filesystem::path& relative_path, std::unique_ptr<std::ofstream>& out_file);
+        boost::system::error_code create_file(const std::filesystem::path& relative_path, uint32_t file_id);
 
         // данный метод принимает file_id, чтобы в будущем можно было держать много файлов открытыми
         boost::system::error_code write(const char* data, std::size_t size, uint32_t file_id);
--- src/utils/file_builder.cpp
+++ src/utils/file_builder.cpp
@@ -25,19 +25,24 @@ boost::system::error_code File_builder::create_directory(const std::filesystem::
     return ec;
 }
 
-boost::system::error_code File_builder::create_file(const std::filesystem::path& relative_path, std::unique_ptr<std::ofstream>& out_file)
+boost::system::error_code File_builder::create_file(const std::filesystem::path& relative_path, uint32_t file_id)
 {
     std::error_code ec;
     auto full_path = root_ / relative_path; // оператор '/' перегружен так что склеивает две строки
     std::filesystem::create_directories(full_path.parent_path(), ec);
     if(ec)
         return ec;
-    out_file = std::make_unique<std::ofstream>(full_path, std::ios::binary | std::ios::trunc);
-    if(!out_file->is_open())
+
+    auto file = std::make_unique<std::ofstream>(full_path, std::ios::binary | std::ios::trunc);
+
+    if(!file->is_open())
     {
-        out_file.reset(); // очистка указателя если че то пошло не так
+        file.reset(); // очистка указателя если че то пошло не так
         return boost::system::errc::make_error_code(boost::system::errc::io_error);
     }
+
+    open_files_[file_id] = std::move(file);
+
     return {};
 }
 
@@ -62,38 +67,4 @@ boost::system::error_code File_builder::close_file(uint32_t file_id)
     it->second->close();
     open_files_.erase(it);
     return {};
-}
-
-boost::system::error_code File_builder::handle_packet(const Packet& packet)
-{
-    switch (packet.header.type)
-    {
-        case PacketType::CONFIRM:
-        case PacketType::CONFIRM_FAILED:
-
-        case PacketType::CREATE_DIRECTORY:
-        {
-            return create_directory(extract_path(packet));
-        }
-
-        case PacketType::CREATE_FILE:
-        {
-            std::unique_ptr<std::ofstream> file;
-            auto ec = create_file(extract_path(packet), file);
-            if(ec)
-                return ec;
-            open_files_[packet.header.file_id] = std::move(file);
-            return {};
-        }
-        case PacketType::FILE_DATA:
-        {
-            return write(packet.data, packet.header.size, packet.header.file_id);
-        }
-        case PacketType::END_FILE:
-
-        default:
-        {
-            return {};
-        }
-    }
 }
