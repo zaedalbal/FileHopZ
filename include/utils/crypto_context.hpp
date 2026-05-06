7e1e47baf56edab70362f0e194486d6d4a2b6cb4
zaedalbal <vokiolhol@gmail.com>
2026-05-06 03:38:58 +0300

    update crypto_context.hpp

--- include/utils/crypto_context.hpp
+++ include/utils/crypto_context.hpp
@@ -21,30 +21,31 @@ class Crypto_context
         Crypto_context(const Crypto_context&) = delete;
         Crypto_context& operator=(const Crypto_context&) = delete;
         
-        // корутина, которая сначала создает собственные private key и public key,
-        // и возобновляется при set_peer_public_key
-        // (после установки чужого ключа можно шифровать и расшифровывать данные)
+        // создает public key и private key
         boost::system::error_code init();
 
-        // шифрует передаваемые данные и возращает unique_ptr на массив с зашифрованными данными
+        // шифрует передаваемые данные и возращает vector с зашифрованными данными
         std::expected<std::vector<char>, boost::system::error_code>
         encrypt_data(const char* data, std::size_t size);
 
-        // расшифровывает передаваемые данные и возращает unique_ptr на массив с расшифрованными данными
+        // расшифровывает передаваемые данные и возращает vector с расшифрованными данными
         std::expected<std::vector<char>, boost::system::error_code>
-        ecrypt_data(const char* data, std::size_t size);
+        decrypt_data(const char* data, std::size_t size);
 
         // возращает собственный public key
         std::span<const char> get_own_public_key();
 
         // устанавливает public key peer'а
-        boost::system::error_code set_peer_public_key(const char* peer_public_key, std::size_t size);
+        // только после установки можно вызывать encrypt_data и decrypt_data
+        boost::system::error_code set_peer_public_key(const char* peer_public_key);
     
     private:
         bool is_ready_ = false;
 
         // указатель на структуру из openssl, которая хранит ключи
         EVP_PKEY* key_handle_ = nullptr;
+
+        std::array<char, KEY_LEN> own_public_key_;
 
         std::array<char, KEY_LEN> encryption_key_;
 };
